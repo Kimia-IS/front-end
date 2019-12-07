ed9bc12787ec8d8ead3cb663d28d450619e0a6a6
alfinm01 <arrestral@gmail.com>
2019-12-07 16:32:16 +0700

    Add RegisterDosen and RegisterAdmin page

---
+++ pages/admin/register.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+export default function RegisterAdmin() {
+  return (
+    <Container>
+      <Box m={3}>
+	      <Grid container spacing={3}>
+	      	<Grid item xs={12}>
+	          <Typography variant="h4" gutterBottom>
+		        Daftar sebagai Admin
+		      </Typography>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Grid item xs={12} md={5}>
+		          <TextField id="name" label="Nama lengkap" variant="outlined" fullWidth />
+		        </Grid>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Grid item xs={12} md={5}>
+		          <TextField id="email" label="Alamat email" variant="outlined" fullWidth />
+		        </Grid>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Grid item xs={12} md={5}>
+		          <TextField id="password" label="Kata sandi" variant="outlined" fullWidth />
+		        </Grid>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Grid item xs={12} md={5}>
+		          <TextField id="re-password" label="Ulangi kata sandi" variant="outlined" fullWidth />
+		        </Grid>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Grid item xs={12} md={5}>
+			      <Button variant="outlined" fullWidth>
+					Masuk
+				  </Button>
+				</Grid>
+	        </Grid>
+	        <Grid item xs={12}>
+		        <Typography variant="subtitle1" gutterBottom>
+		          Sudah punya akun? <a href="/admin/login">Login</a>
+		        </Typography>
+	        </Grid>
+	      </Grid>
+      </Box>
+    </Container>
+  );
+}
