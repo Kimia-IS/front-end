00a8c1b7ad206b448a74ef1117adbdf10ec4bc7e
alfinm01 <arrestral@gmail.com>
2019-12-30 21:56:01 +0700

    setState for tambah/kurang dosen (still error infinite render)

--- pages/akademik/create.js
+++ pages/akademik/create.js
@@ -6,8 +6,54 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default function CreateAkademik() {
+	let openDialog = false;
+	const [count, setCount] = React.useState(1);
+	const [open, setOpen] = React.useState(false); // or use openDialog ? true : false
+
+	const handleClickOpen = () => {
+	    setOpen(true);
+	};
+
+	const handleClose = () => {
+	    setOpen(false);
+	};
+
+	const addFieldDosen = () => {
+		console.log('addFieldDosen')
+		console.log('state = ' + count)
+	    let field = [];
+
+	    if (count >= 3) {
+	    	handleClickOpen;
+	    	{() => {setOpen(true); console.log(open);}} // ga dijalanin
+	    	console.log('setOpen = ' + open)
+	    } else {
+		    for (let i = 2; i < count; i++) {
+		      field.push(
+		      	<Grid item xs={12} md={8}>
+		        	<Grid container spacing={3}>
+				      	<Grid item xs={12} md={7}>
+				          <TextField id={"dosen_" + i} label={"Nama dosen " + i} variant="outlined" required fullWidth />
+				        </Grid>
+				        <Grid item xs={12} md={5}>
+				          <TextField id={"sks_dosen_" + i} label={"SKS dosen " + i} variant="outlined" fullWidth required />
+				        </Grid>
+			        </Grid>
+		        </Grid>
+		      );
+		    }
+	    }
+
+	    return field;
+	 }
+
   return (
     <div>
       <Grid container spacing={3}>
@@ -27,39 +73,58 @@ export default function CreateAkademik() {
 	        Buat Mata Kuliah Baru
 	      </Typography>
         </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="kode_matkul" label="Kode mata kuliah" variant="outlined" fullWidth />
+        <Grid item xs={12} md={3}>
+          <TextField id="kode_matkul" label="Kode mata kuliah" variant="outlined" fullWidth required />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <TextField id="nama_matkul" label="Nama mata kuliah" variant="outlined" fullWidth required />
+        </Grid>
+        <Grid item xs={12} md={8}>
+        	<Grid container spacing={3}>
+		        <Grid item xs={12} md={7}>
+		          <TextField id="dosen_1" label="Nama dosen 1" value="Feby Eliana Tengry" variant="outlined" disabled fullWidth />
+		        </Grid>
+		        <Grid item xs={12} md={5}>
+		          <TextField id="sks_dosen_1" label="SKS dosen 1" variant="outlined" fullWidth required />
+		        </Grid>
+        	</Grid>
+        </Grid>
+        { addFieldDosen() }
+        <Dialog
+	      open={open}
+	      onClose={handleClose}
+	      aria-labelledby="alert-dialog-title"
+	      aria-describedby="alert-dialog-description"
+	    >
+	        <DialogTitle id="alert-dialog-title">{"Oops!"}</DialogTitle>
+	        <DialogContent>
+	          <DialogContentText id="alert-dialog-description">
+	            Maksimal dosen pengajar hanyalah 3 dosen per kelas
+	          </DialogContentText>
+	        </DialogContent>
+	        <DialogActions>
+	          <Button onClick={handleClose} color="primary" autoFocus>
+	            Tutup
+	          </Button>
+	        </DialogActions>
+	    </Dialog>
+        <Grid item xs={12} md={5}>
+        	<Grid container spacing={3}>
+		        <Grid item xs={12} md={6}>
+		        	<Button onClick={() => {setCount(count => count + 1); console.log('set state = ' + count)}}>
+			          + Tambah dosen pengajar
+		        	</Button>
+		        </Grid>
+		        <Grid item xs={12} md={6}>
+		        	<Button onClick={() => setCount(count => count - 1)}>
+			          - Kurangi dosen pengajar
+		        	</Button>
+		        </Grid>
 	        </Grid>
         </Grid>
         <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="nama_matkul" label="Nama mata kuliah" variant="outlined" fullWidth />
-	        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="sks" label="Total SKS" variant="outlined" fullWidth />
-	        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="dosen_1" label="Nama dosen 1" variant="outlined" fullWidth />
-	        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="sks_dosen_1" label="SKS dosen 1" variant="outlined" fullWidth />
-	        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="dosen_2" label="Nama dosen 1" variant="outlined" fullWidth />
-	        </Grid>
-        </Grid>
-        <Grid item xs={12}>
-	        <Grid item xs={12} md={5}>
-	          <TextField id="sks_dosen_3" label="SKS dosen 1" variant="outlined" fullWidth />
+	        <Grid item xs={12} md={4}>
+	          <TextField id="sks" label="Total SKS" value="3" variant="outlined" disabled fullWidth />
 	        </Grid>
         </Grid>
         <Grid item xs={12}>
