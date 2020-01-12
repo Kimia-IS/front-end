8b68bab0ecaa5a8aa2910eab1d222bb7dc76aab1
alfinm01 <arrestral@gmail.com>
2020-01-13 06:05:34 +0700

    Add penelitian

--- pages/penelitian/index.js
+++ pages/penelitian/index.js
@@ -9,6 +9,38 @@ import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default function Index() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'No', field: 'no' },
+      { title: 'Judul', field: 'judul' },
+      { title: 'Nama Dosen', field: 'nama_dosen' },
+      { title: 'Tahun', field: 'tahun' }
+    ],
+    data: [
+      {
+        id: 1,
+        no: 1,
+        judul: 'Pengaruh CO2 terhadap NO2',
+        tahun: 2012,
+        nama_dosen: 'Handajaya Rusli'
+      },
+      {
+        id: 2,
+        no: 2,
+        judul: 'Pengaruh NaOH terhadap CO2',
+        tahun: 2013,
+        nama_dosen: 'Handajaya Rusli'
+      },
+      {
+        id: 3,
+        no: 3,
+        judul: 'Pengaruh H2O terhadap O2',
+        tahun: 2014,
+        nama_dosen: 'Handajaya Rusli'
+      },
+    ],
+  });
+
   const router = useRouter();
 
   return (
@@ -22,11 +54,37 @@ export default function Index() {
             <Typography color="textPrimary">Penelitian</Typography>
           </Breadcrumbs>
         </Grid>
+        <Grid item xs={8} md={2}>
+          <Button variant="outlined" fullWidth href='/penelitian/create'>
+            Buat Penelitian Baru
+          </Button>
+        </Grid>
       </Grid>
       <br />
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          Index
+          <MaterialTable
+            title="Daftar Penelitian"
+            columns={state.columns}
+            data={state.data}
+            actions={[
+              {
+                icon: 'visibility',
+                tooltip: 'See More',
+                onClick: (event, rowData) => { router.push('/penelitian/' + 'id') }
+              },
+              {
+                icon: 'edit',
+                tooltip: 'Edit',
+                onClick: (event, rowData) => { router.push('/penelitian/edit/' + 'id') }
+              },
+              {
+                icon: 'delete',
+                tooltip: 'Delete',
+                onClick: (event, rowData) => { confirm("Apakah Anda yakin ingin menghapus " + rowData.nama_dosen + " - " + rowData.tahun + "?") }
+              }
+            ]}
+          />
         </Grid>
       </Grid>
     </div>
