2765de3e71b9953fbbbe7aad55205ad8afb9c47b
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-01-28 10:40:06 +0700

    Refactor riwayat kerja

--- pages/riwayat-kerja/index.js
+++ pages/riwayat-kerja/index.js
@@ -9,7 +9,7 @@ import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default function Index() {
-  const [state, setState] = React.useState({
+  const [state] = React.useState({
     columns: [
       { title: 'No', field: 'no' },
       { title: 'Nama Pekerjaan', field: 'judul' },
@@ -71,12 +71,12 @@ export default function Index() {
               {
                 icon: 'visibility',
                 tooltip: 'See More',
-                onClick: (event, rowData) => { router.push('/riwayat-kerja/' + 'id'); }
+                onClick: () => { router.push('/riwayat-kerja/' + 'id'); }
               },
               {
                 icon: 'edit',
                 tooltip: 'Edit',
-                onClick: (event, rowData) => { router.push('/riwayat-kerja/edit/' + 'id'); }
+                onClick: () => { router.push('/riwayat-kerja/edit/' + 'id'); }
               },
               {
                 icon: 'delete',
