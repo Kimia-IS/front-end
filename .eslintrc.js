a66dc4f5ab76a017200c280450db9c0131cd0986
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-01-28 07:36:23 +0700

    Installing eslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "eqeqeq": 2,
+        "no-self-compare": 2,
+        "no-useless-concat": 2,
+        "array-bracket-spacing": [2, "never"],
+        "block-spacing": [2, "always"],
+        "comma-spacing": [2, {before: false, after: true}],
+        "camelcase": [2, {"properties": "always"}],
+        "brace-style": [2, "stroustrup", { "allowSingleLine": true }],
+        "comma-style": [2, "last"],
+        "no-var": 2,
+        "consistent-return": 2,
+        "key-spacing": [2, {"beforeColon": false, "afterColon": true}],
+        "semi-spacing": [2, {"before": false, "after": true}],
+        "keyword-spacing": 2,
+        "space-before-function-paren": [2, "never"],
+        "semi": ["error", "always"],
+        "import/no-relative-parent-imports": "error"
+    }
+};
--- pages/profil/id.js
+++ pages/profil/id.js
@@ -17,6 +17,13 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
 import {
@@ -33,8 +40,23 @@ const useStyles = makeStyles(theme => ({
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
+  },
+  table: {
+    minWidth: 650,
   }
 }));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 export default function LihatProfil() {
 	const classes = useStyles();
@@ -128,12 +150,32 @@ export default function LihatProfil() {
 	        >
 	          <Typography className={classes.heading}>Akademik</Typography>
 	        </ExpansionPanelSummary>
-	        <ExpansionPanelDetails>
-	          <Typography>
-	            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-	            sit amet blandit leo lobortis eget.
-	          </Typography>
-	        </ExpansionPanelDetails>
+	        <TableContainer component={Paper}>
+		      <Table className={classes.table} aria-label="simple table">
+		        <TableHead>
+		          <TableRow>
+		            <TableCell>Dessert (100g serving)</TableCell>
+		            <TableCell align="right">Calories</TableCell>
+		            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+		            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+		            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+		          </TableRow>
+		        </TableHead>
+		        <TableBody>
+		          {rows.map(row => (
+		            <TableRow key={row.name}>
+		              <TableCell component="th" scope="row">
+		                {row.name}
+		              </TableCell>
+		              <TableCell align="right">{row.calories}</TableCell>
+		              <TableCell align="right">{row.fat}</TableCell>
+		              <TableCell align="right">{row.carbs}</TableCell>
+		              <TableCell align="right">{row.protein}</TableCell>
+		            </TableRow>
+		          ))}
+		        </TableBody>
+		      </Table>
+		    </TableContainer>
 	      </ExpansionPanel>
 	      <ExpansionPanel>
 	        <ExpansionPanelSummary
